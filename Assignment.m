9c365218a4af05c02ea78849e489cc9de2b7a321
Muhammad H. Al-Aref <muhammad.alaref96@eng-st.cu.edu.eg>
2017-12-22 23:54:52 +0200

    Fixed!

--- Assignment.m
+++ Assignment.m
@@ -4,15 +4,7 @@ m = input_signal_slice;
 energy = (sum(m.^2))/(size(m)(1));
 SNR = 0 : 1 : 20;
 sigma = sqrt(energy ./ (10.^(SNR./10)));
-ModulationIndex = 0.8;
-A = ModulationIndex/max(abs(m));
-plot(SNR,detect_coherent(m,sigma),'-r',SNR,detect_non_coherent(m,sigma,A),'-b');
+plot(SNR,detect_coherent(m,sigma),'-r',SNR,detect_non_coherent(m,sigma),'-b');
 xlabel('SNR');
 ylabel('MSE')
 legend('Coherent Detection','Non Coherent Detection');
-
-disp('Power Efficiency =' );
-power = sum(m.^2) / size(m)(1);
-
-efficiency = (power / (power + (A.^2))) * 100;
-disp(efficiency);
--- detect_coherent.m
+++ detect_coherent.m
@@ -1,29 +1,5 @@
-## Copyright (C) 2017 hassan
-## 
-## This program is free software; you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 3 of the License, or
-## (at your option) any later version.
-## 
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-## 
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-## -*- texinfo -*- 
-## @deftypefn {Function File} {@var{retval} =} detect_coherent (@var{input1}, @var{input2})
-##
-## @seealso{}
-## @end deftypefn
-
-## Author: hassan <hassan@Jumana>
-## Created: 2017-12-22
-
 function [MSE] = detect_coherent (m, sigma)
 z_coherent = randn(size(m)) * sigma;
 r_coherent = m + z_coherent;
-MSE = sum((r_coherent-m).^2)/size(m)(1);
-endfunction
+MSE = sum((r_coherent-m).^2)/length(m);
+end
